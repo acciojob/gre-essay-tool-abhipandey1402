d943424fa901b891ae5f6b7ea1fec70eb1287831
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-06 14:04:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+const evaluatedText = document.getElementById('evaluatedText');
+const wordCount = document.getElementById('wordCount');
+
+evaluatedText.addEventListener('input', () => {
+  const text = evaluatedText.value.trim();
+  if (text === '') {
+    wordCount.textContent = '0';
+  } else {
+    const count = text.split(/\s+/).length;
+    wordCount.textContent = count;
+  }
+});
